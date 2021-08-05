5ad8b5150f39584034fcc04ab452e258188b1f81
Rattin99 <rattin99m18@gmail.com>
2021-08-06 03:28:34 +0600

    persistent menu

--- bottender.config.js
+++ bottender.config.js
@@ -20,7 +20,9 @@ module.exports = {
       },
     },
   },
-  initialState: {},
+  initialState: {
+    arr: [],
+  },
   channels: {
     messenger: {
       enabled: true,
@@ -30,6 +32,27 @@ module.exports = {
       appId: process.env.MESSENGER_APP_ID,
       appSecret: process.env.MESSENGER_APP_SECRET,
       verifyToken: process.env.MESSENGER_VERIFY_TOKEN,
+      profile:{
+        persistentMenu:[
+          {
+            locale: 'default',
+            composerInputDisabled: false,
+            callToActions:[
+              {
+                type: 'postback',
+                title: 'i want to submit Question-Answer',
+                payload: 'submit'
+              },
+              {
+                type: 'web_url',
+                title: 'take me to the questions!',
+                url: 'https://www.notion.so/SUST-CSE-QB-7b86b0c1074b493d84fabceb842244a5',
+                webviewHeightRatio: 'full',
+              }
+            ]
+          }
+        ]
+      }
     },
     whatsapp: {
       enabled: false,
