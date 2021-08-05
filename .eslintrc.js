623f473af5ff5a109a4b14cd850ae0ea4f87bbc0
Rattin99 <rattin99m18@gmail.com>
2021-08-06 03:48:44 +0600

    aa

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,20 +2,20 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2018,
   },
-  // extends: ['eslint:recommended', 'prettier'],
-  // env: {
-  //   node: true,
-  // },
-  // rules: {
-  //   'prettier/prettier': [
-  //     'error',
-  //     {
-  //       trailingComma: 'es5',
-  //       singleQuote: true,
-  //     },
-  //   ],
-  // },
-  // plugins: ['prettier'],
+  extends: ['eslint:recommended', 'prettier'],
+  env: {
+    node: true,
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'es5',
+        singleQuote: true,
+      },
+    ],
+  },
+  plugins: ['prettier'],
   overrides: [
     {
       files: ['**/*.test.js'],
