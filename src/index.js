0e7e17761b4228ca1040e929c692b4f8f3a5f957
Rattin99 <rattin99m18@gmail.com>
2021-08-06 12:19:18 +0600

    routing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-module.exports = async function App(context) {
+const {router, text,payload} = require('bottender/router');
 
- context.sendText("hi");
- 
+module.exports = async function App(context) {
+    return router([
+        payload('SUBMIT',)
+    ])
 };
+
+async function respond(context){
+    context.sendText('dara notion integrate korinai ekhono')
+}
 
 
