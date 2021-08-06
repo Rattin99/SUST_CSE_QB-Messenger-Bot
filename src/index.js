713e82b99b72817addf54cf8c0de3c68fd4cb689
Rattin99 <rattin99m18@gmail.com>
2021-08-06 13:15:20 +0600

    set up route

--- src/index.js
+++ src/index.js
@@ -2,12 +2,40 @@ const {router, text,payload} = require('bottender/router');
 
 module.exports = async function App(context) {
     return router([
-        payload('SUBMIT',respond)
+        payload('SUBMIT',respond),
+
+        payload('QUESTION_POST',handleQuestionPost),
     ])
 };
 
+
 async function respond(context){
-    context.sendText('dara notion integrate korinai ekhono')
+    context.sendText('viva valo hoise asha kori. lets go!',{
+        quickReplies: [
+            {
+                contentType: 'text',
+                title: 'post Question',
+                payload: 'QUESTION_POST'
+            }
+        ]
+    })
+}
+
+async function handleQuestionPost(context){
+    context.sendText('answer ki jana ase? post korte chas?',{
+        quickReplies:[
+            {
+                contentType: 'text',
+                title: 'ha answer post korbo',
+                payload: 'ANSWER_POST'
+            },
+            {
+                contentType: 'text',
+                title: 'na answer jani e na!',
+                payload: 'ANSWER_DECLINE'
+            }
+        ]
+    })
 }
 
 
